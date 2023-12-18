dd06408c7d6cc98124adf55a4bbae18dcd7b35f8
mariam <mariamsalah478@gmail.com>
2023-12-18 22:59:39 +0200

    sum

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello mariam!");
         System.out.println("Hello mariam salah!");
 
+    }
+    int sum(int n1,int n2)
+    {
+        System.out.println("result");
+        System.out.println(n1+n2);
+        return n1+n2;
     }
 }
