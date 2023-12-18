ecc03a6bcc41d9131daf2f0d68281db1476782cd
mariam <mariamsalah478@gmail.com>
2023-12-18 23:12:54 +0200

    add n1,n2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     }
     int sum(int n1,int n2)
     {
-        System.out.println("add");
+        System.out.println("add n1,n2");
         System.out.println(n1+n2);
         return n1+n2;
     }
